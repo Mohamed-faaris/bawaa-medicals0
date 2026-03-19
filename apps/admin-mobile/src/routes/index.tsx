aacd02fac4c09e540ea840fdbf1b5df5bf368fa7
Mohamed Faaris <mohamedfaaris.dev@gmail.com>
2026-03-20 01:48:31 +0530

    cleand

--- apps/admin-mobile/src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/')({
-  component: () => (
-    <div className="p-6 text-2xl font-bold">
-      Admin Mobile App - Bavaa Medicals
-    </div>
-  ),
-})
--- apps/admin-panel/src/routes/__root.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-
-export const Route = createRootRoute({
-  component: () => (
-    <div className="min-h-screen bg-gray-100 text-gray-900">
-      <Outlet />
-    </div>
-  ),
-})
